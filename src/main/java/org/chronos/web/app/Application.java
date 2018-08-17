553b2a1983b2ab16ba6b22ee382e9c0ad403294a
William Zhou <williamkzhou@hotmail.com>
2018-08-17 13:02:09 +0100

    UI components added

--- src/main/java/org/chronos/web/app/Application.java
+++ src/main/java/org/chronos/web/app/Application.java
@@ -1,12 +1,12 @@
 package org.chronos.web.app;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 @ComponentScan(basePackages = {"org.chronos.web.*"})
 public class Application extends SpringBootServletInitializer{
 	public static void main(String[] args) {
